5c429621a5ac24a368f812780491789fd86959f9
M <marilynduf@gmail.com>
2025-09-02 17:30:10 -0400

    Better naming

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,15 @@
 import { useState } from "react";
 import { useRef } from "react";
-
 export default function Player() {
     const playerName = useRef();
-    const [playerNameValue, setInputValue] = useState(null);
+    const [playerNameValue, setPlayerNameValue] = useState(null);
 
     function handleClick() {
-        if (playerName.current.value) {
-            setInputValue(playerName.current.value);
-        }
+        setPlayerNameValue(playerName.current.value);
     }
-
     return (
         <section id="player">
-            <h2>Welcome {playerNameValue ?? "to you"}!</h2>
+            <h2>Welcome {playerNameValue ?? " to you"}!</h2>
             <p>
                 <input ref={playerName} type="text" />
                 <button onClick={handleClick}>Set Name</button>
