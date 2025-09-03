cac32fa9a6e03ea6823f187e43972094b81bf329
M <marilynduf@gmail.com>
2025-09-03 11:06:33 -0400

    Affichage (toggle) texte quand timer est actif and quand pas actif

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 export default function TimerChallenge({ title, targetTime }) {
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+    console.log(timerExpired);
+    function handleStart() {
+        setTimerStarted(true);
+        setTimeout(() => {
+            setTimerExpired(true);
+            setTimerStarted(false);
+        }, targetTime * 1000);
+    }
+
     return (
         <section className="challenge">
             <h2>{title}</h2>
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? "s" : ""}
-                <p>
-                    <button>Start challenge</button>
-                </p>
-                <p className="">Timee is running / Timer inactive</p>
+            </p>
+            <p>
+                <button onClick={handleStart}>
+                    {timerStarted ? "Stop" : "Start"}
+                </button>
+            </p>
+            <p className={timerStarted ? "active" : ""}>
+                {timerStarted ? "Timer is running" : ""}
             </p>
         </section>
     );
